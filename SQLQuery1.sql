8c46138d6d540672bd746a41d3a6521ad25e1e3e
Sean Sculley <doneotreply@github.com>
2018-08-21 14:53:54 -0400

    addedlab20

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -27,6 +27,51 @@ Select * From Orders Where ShipRegion IS NOT NULL
 
 Select * From Customers Order By Country, City
 
---Add Customer to Customer table
+--Add Customer to Customer table (come back)
+
+Update Orders Set ShipRegion = 'EuroZone' Where ShipCountry = 'France'
+
+
+--Delete all orders that have quantity 1
+
+Delete From [Order Details] Where Quantity = 1
+
+--Find Average MIN and Max for
+
+Select Min(Quantity) From [Order Details]
+Select Max(Quantity) From [Order Details]
+Select AVG(Quantity) From [Order Details]
+
+--Find Average MIN and Max for group by OrderID
+
+Select Min(Quantity) From [Order Details] Group by OrderID
+Select Max(Quantity) From [Order Details] Group by OrderID
+Select AVG(Quantity) From [Order Details] Group by OrderID
+
+--Select customer for order ID 10290
+
+Select CustomerID From [Orders] Where OrderID = '10290'
+
+--Q14
+
+SELECT Orders.OrderID, Customers.CustomerID 
+FROM (Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+
+SELECT Orders.OrderID, Customers.CustomerID 
+FROM (Orders
+Left JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+
+SELECT Orders.OrderID, Customers.CustomerID 
+FROM (Orders
+Right JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+
+--Q15
+Select FirstName From [Employees] Where ReportsTo IS NULL
+
+--Q16
+
+SELECT FirstName FROM [Employees] WHERE ReportsTo = 2
+
 
 
